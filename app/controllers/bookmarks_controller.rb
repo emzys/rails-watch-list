10e16d2db39960104ac6c316bfa7766f38b4b9b1
Emilia Zysk <emzysk.dev@gmail.com>
2021-05-20 18:34:34 +0900

    add actions to bookmark controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,34 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    # @movies = Movie.where(id: @list.movies).order(:name)
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @movie = Movie.find(bookmark_params[:movie_id])
+    p @movie
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      # @movies = Movie.where(id: @list.movies).order(:name)
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
