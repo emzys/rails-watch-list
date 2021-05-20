f260769d3ee48aa846d7c158cb314dbd50b93475
Emilia Zysk <emzysk.dev@gmail.com>
2021-05-21 00:03:20 +0900

    better structure and styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,9 @@ ActiveStorage.start()
 
 import 'bootstrap';
 
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
